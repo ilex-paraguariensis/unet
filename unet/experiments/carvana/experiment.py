5402de71ba8a850c125d65b4fd9691cf8d1d877d
GiulioZani <giulio.zani@gmail.com>
2022-11-22 10:31:10 +0100

    update

--- unet/experiments/carvana/experiment.py
+++ unet/experiments/carvana/experiment.py
@@ -3,19 +3,19 @@ from ...trainers.unet_trainer.trainer import train
 from ...trainers.unet_trainer.evaluate import test
 from ...models.unet.unet_model import UNet
 from ...models.unet.pretrained_unet import pretrained_net
-import torch
+import sys
 
 # save_dir = "../save_dir/"
 net = UNet(n_channels=3, n_classes=2)
 train_dataset = CarvanaDataset(images_dir="./train", masks_dir="./train_masks")
 test_dataset = CarvanaDataset(images_dir="./train", masks_dir="./train_masks")
-returns = {
-    "train": [
-        train(
-            net=net,
-            dataset=train_dataset,
-        )
-    ],
-    "test": [test(model_class=UNet, dataset=test_dataset)],
-    "restart": [],
-}
+
+command = sys.argv[1]
+
+if command == "train":
+    train(
+        net=net,
+        dataset=train_dataset,
+    )
+elif command == "test":
+    test(model_class=UNet, dataset=test_dataset)
